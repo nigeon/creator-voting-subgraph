aee9897f5029c9ccd61bd513b04104156411d562
Aleix <nigeon@gmail.com>
2021-09-24 21:37:50 +0200

    Rebuilt

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,62 +1,97 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  VotingFactory,
-  AdminChanged,
-  BeaconUpgraded,
-  Initialized,
-  Upgraded,
-  VotingProcessDeployed
-} from "../generated/VotingFactory/VotingFactory"
-import { ExampleEntity } from "../generated/schema"
+import { Address, DataSourceContext } from '@graphprotocol/graph-ts'
+import { VotingProcessDeployed } from "../generated/VotingFactory/VotingFactory"
+import { Voted } from "../generated/templates/VotingProcess/VotingProcess"
+import { Answer, Token, TokenVotingProcess, User, Vote, VotingProcess } from "../generated/schema"
+import { VotingProcess as VotingProcessTemplate } from "../generated/templates"
 
-export function handleAdminChanged(event: AdminChanged): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
+export function handleVotingProcessDeployed(event: VotingProcessDeployed): void {
+  let entity = VotingProcess.load(event.params.votingProcessAddress.toHex());
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new VotingProcess(event.params.votingProcessAddress.toHex());
   }
+  
+  entity.user = createUser(event.params.creator).id;
+  entity.contract = event.params.votingProcessAddress;
+  entity.question = event.params.question;
+  entity.description = event.params.description;
+  entity.uri = event.params.uri;
+  entity.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let context = new DataSourceContext();
+  context.setBytes('contract', event.params.votingProcessAddress);
+  VotingProcessTemplate.createWithContext(event.params.votingProcessAddress, context);
 
-  // Entity fields can be set based on event parameters
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.createVotingProcess(...)
+  let answers = event.params.answers;
+  for(let i=0; i<answers.length; i++){
+    createAnswer(entity.id, i, answers[i]);
+  }
+  
+  let acceptedERC20 = event.params.acceptedERC20;
+  for(let i=0; i<acceptedERC20.length; i++){
+    let createdToken = createToken(entity.id, acceptedERC20[i]);
+    createTokenVotingProcess(createdToken.id, entity.id);
+  }
 }
 
-export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
+export function handleVoted(event: Voted): void {
+  let entity = Vote.load(event.transaction.hash.toHex());
+  if (entity == null) {
+    entity = new Vote(event.transaction.hash.toHex());
+  }
 
-export function handleInitialized(event: Initialized): void {}
+  entity.user = createUser(event.transaction.from).id;
+  entity.votingProcess = event.transaction.to.toHex();
+  entity.answer = Answer.load(event.transaction.to.toHex() + event.params.answerId.toString()).id;
+  entity.votingToken = Token.load(event.transaction.to.toHex() + event.params.votingToken.toHex()).id
+  entity.votingAmount = event.params.votingAmount;
+  entity.save(); 
+}
 
-export function handleUpgraded(event: Upgraded): void {}
+export function createUser(address: Address): User {
+  let user = User.load(address.toHexString());
+  if (user === null) {
+    user = new User(address.toHexString());
+    user.address = address;
+    user.save();
+  }
 
-export function handleVotingProcessDeployed(
-  event: VotingProcessDeployed
-): void {}
+  return user as User;
+}
+
+export function createAnswer(votingProcessId: string, index: number, ans: string): Answer {
+  let generatedId = votingProcessId.toString() + index.toString();
+  let answer = Answer.load(generatedId);
+  if (answer === null) {
+    answer = new Answer(generatedId);
+    answer.votingProcess = votingProcessId;
+    answer.answer = ans;
+    answer.save();
+  }
+
+  return answer as Answer;
+}
+
+export function createToken(votingProcessId: string, acceptedToken: Address): Token {
+  let generatedId = votingProcessId + acceptedToken.toHex();
+  let token = Token.load(generatedId);
+  if (token === null) {
+    token = new Token(generatedId);
+    token.address = acceptedToken;
+    token.save();
+  }
+
+  return token as Token;
+}
+
+export function createTokenVotingProcess(createdTokenId: string, votingProcessId: string): TokenVotingProcess {
+  let generatedId = votingProcessId + createdTokenId;
+  let tokenVotingProcess = TokenVotingProcess.load(generatedId);
+  if (tokenVotingProcess === null) {
+    tokenVotingProcess = new TokenVotingProcess(generatedId);
+    tokenVotingProcess.votingProcess = votingProcessId;
+    tokenVotingProcess.token = createdTokenId;
+    tokenVotingProcess.save();
+  }
+
+  return tokenVotingProcess as TokenVotingProcess;
+}
